15072a436761e05775a73fb27fcd22dbaaa79cdc
Extaz4you <nikitqalevioffanov@gmaol.com>
2022-02-08 18:33:43 +0300

    save

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿int num = 10;
+int y = 1;
+int x = 1;
+while (x <= num)
+{
+     Console.Write(x+ "\t");
+     x++;
+}
+Console.WriteLine();
+while (y <= num)
+{
+     Console.Write(y*y+ "\t");
+     y++;
+}
+
+
+//int N = 10;
+//int count = 1;
+//while (count <= N)
+{
+    //Console.Write("Число: " + count);
+   // Console.WriteLine();
+    //Console.WriteLine("Квадрат: " + count*count);
+    //Console.WriteLine();
+    //count++;
+}
